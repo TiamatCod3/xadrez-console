4dc9cb5edbebf199eafda685940d4f5e5d988e5b
Tiago Matos <ti_matos@id.uff.br>
2021-09-10 10:49:51 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+using tabuleiro;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+
+    Posicao P;
+
+    P = new Posicao (3,4);
+
+    Console.WriteLine("Posicao: " + P);
+
+    Console.ReadLine();
+
+  }
+}
